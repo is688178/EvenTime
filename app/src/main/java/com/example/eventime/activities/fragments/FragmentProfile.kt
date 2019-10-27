86e2a059b007489a98e80480796ea322267dabe0
is688178 <is688178@gmail.com>
2019-10-27 15:40:54 -0600

    Profile Data connected with PARSE

--- app/src/main/java/com/example/eventime/activities/fragments/FragmentProfile.kt
+++ app/src/main/java/com/example/eventime/activities/fragments/FragmentProfile.kt
@@ -7,13 +7,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.eventime.R
 import com.example.eventime.activities.activities.LogoutListener
+import com.parse.ParseFile
+import com.parse.ParseUser
+import org.jetbrains.anko.find
+import org.jetbrains.anko.image
 
 class FragmentProfile : Fragment(), View.OnClickListener {
     private lateinit var listener: LogoutListener
     private lateinit var container: Context
+    private lateinit var mEmail: TextView
+    private lateinit var mUserName: TextView
+    private lateinit var mImageView: ImageView
+    private lateinit var mCloseSession: Button
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -30,8 +41,17 @@ class FragmentProfile : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
-        view.findViewById<Button>(R.id.fragment_profile_btn_logout).setOnClickListener(this)
         this.container = container!!.context
+
+        mEmail = view.find(R.id.fragment_profile_tv_email)
+        mUserName = view.find(R.id.fragment_profile_tv_user_name)
+        mCloseSession = view.find(R.id.fragment_profile_btn_logout)
+        mImageView = view.find(R.id.ic_user)
+
+        serUserImage()
+        setUserName()
+        setUserEmail()
+        mCloseSession.setOnClickListener(this)
 
         return view
     }
@@ -40,6 +60,37 @@ class FragmentProfile : Fragment(), View.OnClickListener {
         when (view?.id) {
             R.id.fragment_profile_btn_logout -> {
                 listener.logout()
+            }
+        }
+    }
+
+    private fun serUserImage() {
+        if (ParseUser.getCurrentUser() != null) {
+            try {
+                val parseFile: ParseFile = ParseUser.getCurrentUser().get("image") as ParseFile
+                Glide.with(this).load(parseFile.url).into(mImageView)
+            } catch (exception: Exception) {
+                Log.e("DEBUG User Name", exception.message.toString())
+            }
+        }
+    }
+
+    private fun setUserName() {
+        if (ParseUser.getCurrentUser() != null) {
+            try {
+                mUserName.text = ParseUser.getCurrentUser().username
+            } catch (exception: Exception) {
+                Log.e("DEBUG User Name", exception.message.toString())
+            }
+        }
+    }
+
+    private fun setUserEmail() {
+        if (ParseUser.getCurrentUser() != null) {
+            try {
+                mEmail.text = ParseUser.getCurrentUser().email
+            } catch (exception: Exception) {
+                Log.e("DEBUG User Email", exception.message.toString())
             }
         }
     }
