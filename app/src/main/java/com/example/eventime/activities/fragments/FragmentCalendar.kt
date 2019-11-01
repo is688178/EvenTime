b479735ceb27e9901179fdd0c7ebad9c8bc24dcf
Luis_Rod_Val <luis.rodriguez@bitcon.com.mx>
2019-11-01 16:50:48 -0600

    commit to pull new changes

--- app/src/main/java/com/example/eventime/activities/fragments/FragmentCalendar.kt
+++ app/src/main/java/com/example/eventime/activities/fragments/FragmentCalendar.kt
@@ -1,14 +1,16 @@
 package com.example.eventime.activities.fragments
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.CalendarContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-//import com.example.eventime.R
 import kotlinx.android.synthetic.main.fragment_calendar_event.view.*
 import java.util.*
 
@@ -20,10 +22,13 @@ class FragmentCalendar : Fragment(){
     private lateinit var beginTime: Calendar
     private lateinit var endTime: Calendar
 
+    companion object {
+        const val CREATE_EVENT = 1000
+    }
+
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(com.example.eventime.R.layout.fragment_calendar_event, container, false)
         this.container = container!!.context
-        //view.calendar.selectedDates
         view.calendar.setOnDateChangeListener{ view, year, month, dayOfMonth ->
             // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
             msg = ("$dayOfMonth-"+(month + 1)+"-$year")
@@ -32,61 +37,72 @@ class FragmentCalendar : Fragment(){
 
             endTime = Calendar.getInstance()
             endTime.set(year, month, dayOfMonth, 13, 0)
-            //println(msg)
-
-            /*val intent = Intent(Intent.ACTION_INSERT)
-                .setData(CalendarContract.Events.CONTENT_URI)
-                .putExtra(CalendarContract.Events.TITLE, "New Event")
-                .putExtra(CalendarContract.Events.EVENT_LOCATION, "Location")
-                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
-                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60*60*1000))
-            startActivity(intent)*/
 
         }
 
         view.fragment_calendar_add_event.setOnClickListener {
-            print("BEGIN*****************************"+beginTime)
-            print("END********************************"+endTime)
             val intent = Intent(Intent.ACTION_INSERT)
                 .setData(CalendarContract.Events.CONTENT_URI)
                 .putExtra(CalendarContract.Events.TITLE, "New Event")
                 .putExtra(CalendarContract.Events.EVENT_LOCATION, "Location")
                 .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime) //System.currentTimeMillis())
                 .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime)//System.currentTimeMillis() + (60*60*1000))
-            startActivity(intent)
 
-            //startActivityFor Result// ver que regresa
-            //onActivityforResult
+            startActivityForResult(intent, CREATE_EVENT)
         }
-
-        /*val beginTime = Calendar.getInstance()
-        beginTime.set(2012, 0, 19, 7, 30)
-
-        val endTime = Calendar.getInstance()
-        endTime.set(2012, 0, 19, 8, 30)
-
-        val intent = Intent(Intent.ACTION_INSERT)
-            .setData(Events.CONTENT_URI)
-            .putExtra(
-                CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                beginTime.getTimeInMillis()
-            )
-            .putExtra(
-                CalendarContract.EXTRA_EVENT_END_TIME,
-                endTime.getTimeInMillis()
-            )
-            .putExtra(Events.TITLE, "Yoga")
-            .putExtra(Events.DESCRIPTION, "Group class")
-            .putExtra(Events.EVENT_LOCATION, "The gym")
-            .putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY)
-            .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com")
-
-        startActivity(intent)*/
-
-
 
         return view
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode) {
+            CREATE_EVENT -> {
+                when(resultCode) {
+                    RESULT_OK -> print(data)
+                    RESULT_CANCELED -> Log.d("TAG", "CANCELED")
+                }
+            }
+        }
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*val beginTime = Calendar.getInstance()
+       beginTime.set(2012, 0, 19, 7, 30)
+
+       val endTime = Calendar.getInstance()
+       endTime.set(2012, 0, 19, 8, 30)
+
+       val intent = Intent(Intent.ACTION_INSERT)
+           .setData(Events.CONTENT_URI)
+           .putExtra(
+               CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+               beginTime.getTimeInMillis()
+           )
+           .putExtra(
+               CalendarContract.EXTRA_EVENT_END_TIME,
+               endTime.getTimeInMillis()
+           )
+           .putExtra(Events.TITLE, "Yoga")
+           .putExtra(Events.DESCRIPTION, "Group class")
+           .putExtra(Events.EVENT_LOCATION, "The gym")
+           .putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY)
+           .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com")
+
+       startActivity(intent)*/
